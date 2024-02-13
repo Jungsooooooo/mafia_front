d761fb0bb21bbb79576352edf83f80db1edabb32
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-02-13 16:05:15 +0900

    route 주소 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./login/Login";
+import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
