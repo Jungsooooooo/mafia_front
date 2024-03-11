9a956b2b673582fb899032f44da7ad996a0a604f
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-11 10:21:38 +0900

    redux-persist 적용 완료

--- src/index.js
+++ src/index.js
@@ -9,27 +9,20 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./reducers";
 
-const enhancer =
-  process.env.NODE_ENV === "production"
-    ? compose(applyMiddleware())
-    : composeWithDevTools(applyMiddleware(logger));
+const store = createStore(rootReducer);
+const persistor = persistStore(store);
 
-// 위에서 만든 reducer를 스토어 만들때 넣어줍니다
-const store = createStore(rootReducer, enhancer);
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  // 만든 store를 앱 상위에 넣어줍니다.
+const Root = () => (
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Root />, document.getElementById("root"));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,22 @@
 import { combineReducers } from "redux";
 import loginSession from "./loginSession";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 // 여러 reducer를 사용하는 경우 reducer를 하나로 묶어주는 메소드입니다.
 // store에 저장되는 리듀서는 오직 1개입니다.
+
+const persistConfig = {
+  key: "root",
+  // localStorage에 저장합니다.
+  storage,
+  // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
+  whitelist: ["loginSession"],
+  // blacklist -> 그것만 제외합니다
+};
+
 const rootReducer = combineReducers({
   loginSession,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
