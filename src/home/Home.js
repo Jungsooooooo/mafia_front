6b15a585809d7425f5d3465d9a71a4bf70332059
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-11 17:18:20 +0900

    임시 로그아웃 버튼 기능 구현

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import Button from "@mui/material/Button";
+import { logout } from "../reducers/loginSession";
+
 const Home = () => {
-  return <div>home</div>;
+  const dispatch = useDispatch();
+  const isLogged = useSelector((state) => state.loginSession);
+
+  const handleLogout = () => {
+    localStorage.removeItem(isLogged.id);
+    dispatch(logout());
+  };
+
+  return (
+    <div>
+      home
+      <Button onClick={handleLogout}>로그아웃</Button>
+    </div>
+  );
 };
 
 export default Home;
