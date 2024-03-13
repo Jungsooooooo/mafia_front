9482dc04a8d512b164d530659788e7bb805b1122
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-13 17:06:39 +0900

    사용하지 않는 import 삭제

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import { createRoot } from "react-dom/client";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
+
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
