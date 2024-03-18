9156a4eab754fbf1da323c168e0493c3307f7a6a
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-18 17:28:59 +0900

    공통으로 사용할 수 있는 모달창 구현

---
+++ src/common/DialogModal.js
@@ -0,0 +1,50 @@
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Button } from "@mui/material";
+
+import { useState, useEffect } from "react";
+
+const DialogModal = (props) => {
+  const [open, setOpen] = useState(props.open);
+  console.log(props);
+
+  useEffect(() => {
+    if (props.open === true) {
+      handleModal();
+    }
+  }, [props.open]);
+
+  const handleModal = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {props.info}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>확인</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default DialogModal;
