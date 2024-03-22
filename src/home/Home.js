5473089278664de70ace0ea8d7fa402a552f1cf8
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-22 15:00:50 +0900

    채팅방 만들기 기능 추가

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Button from "@mui/material/Button";
 import { logout } from "../reducers/loginSession";
 
+import axios from "axios";
+
 const Home = () => {
   const dispatch = useDispatch();
   const isLogged = useSelector((state) => state.loginSession);
@@ -11,10 +13,38 @@ const Home = () => {
     dispatch(logout());
   };
 
+  const handleMakeRoom = () => {
+    const room = {
+      name: "test1",
+    };
+    axios
+      .post("/api/chat", room, {
+        headers: { Authorization: "Bearer " + isLogged.token },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  const handlecheckRoom = () => {
+    const room = {
+      name: "test2",
+    };
+    axios
+      .get("/api/chat", {
+        headers: { Authorization: "Bearer " + isLogged.token },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div>
       home
       <Button onClick={handleLogout}>로그아웃</Button>
+      <Button onClick={handleMakeRoom}>방만들기</Button>
+      <Button onClick={handlecheckRoom}>방확인</Button>
     </div>
   );
 };
