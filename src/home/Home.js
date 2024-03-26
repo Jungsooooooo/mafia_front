343a86bd0c675cae1ea181414c0a395751bff787
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-03-26 16:28:23 +0900

    만든 방 확인해보는 기능 추가

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import Button from "@mui/material/Button";
 import { logout } from "../reducers/loginSession";
+import { Box, Paper } from "@mui/material";
 
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+import "../css/Home.css";
 
 const Home = () => {
   const dispatch = useDispatch();
   const isLogged = useSelector((state) => state.loginSession);
+
+  const [roomCount, setRoomCount] = useState([]);
+
+  useEffect(() => {});
 
   const handleLogout = () => {
     localStorage.removeItem(isLogged.id);
@@ -35,17 +43,42 @@ const Home = () => {
         headers: { Authorization: "Bearer " + isLogged.token },
       })
       .then((res) => {
-        console.log(res);
+        setRoomCount(res.data);
       });
   };
 
   return (
-    <div>
-      home
-      <Button onClick={handleLogout}>로그아웃</Button>
-      <Button onClick={handleMakeRoom}>방만들기</Button>
-      <Button onClick={handlecheckRoom}>방확인</Button>
-    </div>
+    <>
+      <div>
+        home
+        <Button onClick={handleLogout}>로그아웃</Button>
+        <Button onClick={handleMakeRoom}>방만들기</Button>
+        <Button onClick={handlecheckRoom}>방확인</Button>
+      </div>
+      <div>
+        {roomCount.map((room, index) => {
+          return (
+            <div>
+              <Paper
+                key={room.roomid}
+                sx={{
+                  width: 200,
+                  height: 100,
+                  borderRadius: 1,
+                  bgcolor: "primary.main",
+                  "&:hover": {
+                    bgcolor: "primary.dark",
+                  },
+                  margin: "10px",
+                }}
+              >
+                <p className="gameroom">{room.name}</p>
+              </Paper>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
